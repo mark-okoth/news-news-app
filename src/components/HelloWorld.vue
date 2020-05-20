5be3847fab0e89429add0eda72353a7d759d9646
mark odhiambo okoth <markokoth96@gmail.com>
2020-05-20 10:47:55 +0300

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div v-for="article in articles" :key="article.id">
+      <h3>{{article.author}}</h3>
+      <h6>{{article.content}}</h6>
+      <p>{{article.url}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "news app",
+      articles:'articles',
+      image: "image",
+      url ="url"
+    };
+  },
+  mounted(){
+     axios.get('http://newsapi.org/v2/top-headlines?' +'country=us&'+'apiKey=4b8b39c67c384341a2fcad0f266e0efd')
+     .then(res => {
+       console.log(res.data)
+     this.articles = res.data.articles
+     })
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+img {
+  width: 30vw;
+  height: 30vh;
+}
+</style>
